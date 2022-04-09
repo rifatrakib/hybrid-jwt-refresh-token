431c2fbdb4ec0699e386c97b855160e2ede19f55
marakib1508 <rakib@softwrd.ai>
2022-04-09 16:46:11 +0600

    login and logout implemented and corresponding token creation and removal

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, session, request
+from auth.utils import (
+    create_refresh_token, populate_session,
+    remove_token_records, create_access_token)
+from auth.auth_logic import validate_login, store_token_in_database
+from auth.decorators import token_required
+from flask import Flask, session, request, jsonify
 import os
 
 app = Flask(__name__)
@@ -25,3 +30,55 @@ def clear_session(response):
         response.delete_cookie('session')
     
     return response
+
+
+@app.route('/login', methods=['POST'])
+def user_login():
+    response = {}
+    try:
+        data = request.form
+        user, response = validate_login(data)
+        
+        if user:
+            populate_session(user)
+            response = jsonify(response)
+            session['refresh_token'] = str(create_refresh_token(session['id']))
+            session_data = dict(session)
+            token = create_access_token(session_data)
+            store_data_tokens = {
+                'refresh_token': session['refresh_token'],
+                'access_token': str(token),
+                'user_id': session['user_id'],
+            }
+            token_stats = store_token_in_database(store_data_tokens)
+            response.set_cookie('auth_token', str(token), max_age=int(refresh_token_life))
+            return response
+    
+    except Exception as e:
+        response['error'] = str(e)
+        response['success'] = False
+        user = None
+    
+    finally:
+        return jsonify(response)
+
+
+@app.route('/logout')
+@token_required
+def user_logout():
+    response = {}
+    try:
+        param = session['refresh']
+        response = remove_token_records(param)
+        for key in list(session.keys()):
+            session.pop(key)
+        session.clear()
+        
+        response['logged_out'] = 'Logout successful'
+        response['success'] = True
+        session['logged_out'] = True
+    except Exception as e:
+        response['error'] = str(e)
+        response['success'] = False
+    finally:
+        return jsonify(response)
--- auth/auth_logic.py
+++ auth/auth_logic.py
@@ -1,4 +1,7 @@
+from urllib import response
 from db_instances import db_connection, Refresh_Tokens, User
+from datetime import datetime
+import bcrypt
 
 
 def populate_session_from_database(user_id):
@@ -13,6 +16,33 @@ def populate_session_from_database(user_id):
         response['message'] = 'Something went wrong'
         response['error'] = str(e)
         return response
+
+
+def store_token_in_database(data):
+    response = {}
+    
+    try:
+        session = db_connection()
+        session.add(Refresh_Tokens(
+            REFRESH_TOKEN           = data['refresh_token'],
+            ACCESS_TOKEN            = data['access_token'],
+            USER_ID                 = data['user_id'],
+            IS_REFRESH_EXPIRED      = 0,
+            IS_ACCESS_EXPIRED       = 0,
+            DATE_AT                 = datetime.now()
+        ))
+        session.flush()
+        session.commit()
+        response['success'] = True
+    
+    except Exception as e:
+        response['message'] = 'Something went wrong'
+        response['error'] = str(e)
+        response['success'] = False
+    finally:
+        db_connection.remove()
+    
+    return response
 
 
 def set_new_access_token(expired_access_token, new_token):
@@ -76,3 +106,54 @@ def validate_refresh_token(refresh_token):
         db_connection.remove()
     
     return response
+
+
+def get_user_information(email, password):
+    response = {}
+    try:
+        session = db_connection()
+        user = session.query(User)\
+            .filter(User.EMAIL==email, User.IS_VERIFIED==1).first()
+        
+        if not user:
+            is_verfied_status = session.query(User.IS_VERIFIED)\
+                .filter(User.EMAIL==email).first()
+            
+            if is_verfied_status == None:
+                is_verfied_status_number = 100
+            else:
+                is_verfied_status_number = 0
+            
+            if is_verfied_status_number == 0:
+                status = 'Email has not verified yet!'
+            else:
+                status = 'Invalid email or password'
+    
+    except Exception as e:
+        user = None
+        response['error'] = str(e)
+        response['success'] = False
+        response['message'] = 'Something went wrong'
+        return user, response
+    finally:
+        db_connection.remove()
+    
+    if user:
+        hashed_password = user.PASSWORD
+        if user.PASSWORD != bcrypt.hashpw(password.encode('UTF_8'), hashed_password.encode('UTF_8')).decode():
+            response = {'error': 'Invalid email or password', 'success': False}
+            user = None
+        else:
+            response = {'result': 'Login successful', 'success': True}
+    else:
+        response = {'error': status, 'success': False}
+        user = None
+    
+    return user, response
+
+
+def validate_login(data):
+    email = data['email']
+    password = data['password']
+    user, response = get_user_information(email, password)
+    return user, response
